b77d093886edcc1e111b253f08e4046220f7619f
Ronald Dias <ronaldsilva850@gmail.com>
2024-09-22 23:09:22 -0300

    initial commit

---
+++ php_com_pdo/index.php
@@ -0,0 +1,24 @@
+<?php
+
+$dsn = 'mysql:host=localhost; dbname=php_com_pdo';
+$usuario = 'root';
+$senha = '';
+
+try {
+    $conexao = new PDO($dsn, $usuario, $senha);
+
+    $query = '
+            create table if not exists tb_usuarios(
+                id int not null primary key auto_increment,
+                nome varchar(50) not null,
+                email varchar(50) not null,
+                senha varchar(32) not null
+            )
+    ';
+
+    $retorno = $conexao->exec($query);
+
+} catch (PDOException $e) {
+    echo 'Erro: '. $e->getCode(). 'Mensagem: '. $e->getMessage();
+}
+
